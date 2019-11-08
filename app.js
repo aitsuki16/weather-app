f881db63a1c6b90f365bb7ff74d7fcaf650bcb2a
aitsuki16 <aitsuki26@gmail.com>
2019-11-08 23:01:03 +0900

    ai chan yoku ganbatta and nobuchan modified

--- app.js
+++ app.js
@@ -12,48 +12,48 @@ window.addEventListener('load', () => {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-                long = position.coords.longitude;
-                lat = position.coords.latitude;
-                const proxy = 'http://cors-anywhere.herokuapp.com/';
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const proxy = 'http://cors-anywhere.herokuapp.com/';
 
-                const api = `${proxy}https://api.darksky.net/forecast/9e4f9d5da943d594370ef3962489d34a/${lat},${long}`;
+            const api = `${proxy}https://api.darksky.net/forecast/9e4f9d5da943d594370ef3962489d34a/${lat},${long}`;
 
-                fetch(api)
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(data => {
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
 
-                            const { temperature, summary, icon } = data.currently;
+                    const { temperature, summary, icon } = data.currently;
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    let celcius = (temperature - 32) * (5 / 9);
+                    setIcons(icon, document.querySelector(".icon"));
+
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celcius);
+                        } else {
+                            temperatureSpan.textContent = "F";
                             temperatureDegree.textContent = temperature;
-                            temperatureDescription.textContent = summary;
-                            locationTimezone.textContent = data.timezone;
-                            let celcius = (temperature - 32) * (5 / 9);
-                            setIcons(icon, document.querySelector(".icon"));
 
-                            temperatureSection.addEventListener('click', () => {
-                                    if (temperatureSpan.textContent === "F") {
-                                        temperatureSpan.textContent = "C";
-                                        temperatureDegree.textContent = Math.floor(celsius);
-                                    } else {
-                                        temperatureSpan.textContent = "F";
-                                        temperatureDegree.textContent = temperature;
+                        }
+                    })
 
-                                    })
-                            })
-
-                    });
+                });
 
 
         });
 
-}
+    }
 
-function setIcons(icon, iconID) {
-    const skycons = new Skycons({ color: "white" });
-    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-    skycons.play();
-    return skycons.set(iconID, Skycons[currentIcon]);
-}
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 
 });
